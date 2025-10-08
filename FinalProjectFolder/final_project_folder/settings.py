842530cdb60b2eb59a89747edf278c2a1bf9057a
KortSchaefer <schaeferkort@gmail.com>
2025-10-08 18:45:56 -0500

    Added config for python anywhere

--- FinalProjectFolder/final_project_folder/settings.py
+++ FinalProjectFolder/final_project_folder/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,12 +21,14 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-)m1%3lffx2q5+n@wa_b-p(s14)6l2)cjd^*kb7hp^rh0k67@c3'
+SECRET_KEY = os.getenv("DJANGO_SECRET_KEY", "CHANGE_ME")
+
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.getenv("DJANGO_DEBUG", "0") == "1"
 
-ALLOWED_HOSTS = ['localhost', '127.0.0.1', '.pythonanywhere.com']
+ALLOWED_HOSTS = ['localhost', '127.0.0.1', 'TeamSheetWebsite.pythonanywhere.com']
+CSRF_TRUSTED_ORIGINS = ["https://TeamSheetWebsite.pythonanywhere.com"]
 
 
 # Application definition
